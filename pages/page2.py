1ff589144270125df399847e722250a3109a1560
calazansiesb <EWERTON.CALAZANS@IESB.EDU.BR>
2025-05-17 17:55:03 -0300

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 st.title("Matrizes por Unidade Territorial: Estados e Regiões")
 
